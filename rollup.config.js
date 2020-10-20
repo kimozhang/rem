eb050924972f98482f97ad00ab3c74760e0e9f3d
kimozhang <1193055507@qq.com>
2020-10-20 14:13:17 +0800

    init

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve'
-import babel from '@rollup/plugin-babel'
 import ts from 'rollup-plugin-typescript2'
-import typescript from 'typescript'
 import { terser } from 'rollup-plugin-terser'
-
-import { DEFAULT_EXTENSIONS } from '@babel/core'
 
 const rollupConfig = {
   input: 'src/index.ts',
