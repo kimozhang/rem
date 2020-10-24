91c55c5a8ebc39dbbe6d359b9c08f6e7832b20e8
kimozhang <1193055507@qq.com>
2020-10-24 21:01:32 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
 /**
- * 通过 rem 使页面和设计图等比例缩放
+ * implemention logic:
+ * 1. formula: rem / clientWidth = base / designWidth
+ * 2. When base is set to 100, you can get the CSS value in rem very simply, 
+ *    which is calculated by value of design / 100。 
  * 
- * 实现逻辑
- * 1. 将基数设置成 100，通过公式 rem / clientWidth = 100 / 设计图宽，得出 rem.
- * 2. css 中的值就是 实际的长度 / 100，加上单位 rem. 如：
- *  div { width: 1rem } 1rem = (设计图的实际尺寸 / 100)rem.
- *  通过将基数设置成 100，可以方便的计算出 css 的大小
+ * E.g: there is a div element which is 75px in design, 
+ * then the actual size is 0.75rem by 75 / 100
  * 
  */
 
 import debounce from './debounce'
 
 interface RemOptions {
+  base?: number,
   designWidth?: number,
   maxWidth?: number
 }
 
 const DEFAULT_OPTIONS = {
+  base: 100,
   designWidth: 750
 } 
 
 function init(options: RemOptions = {}) {
-  const opts: RemOptions = Object.assign({}, DEFAULT_OPTIONS, options)
+  const { base, designWidth, maxWidth } = Object.assign({}, DEFAULT_OPTIONS, options)
   const $doc = document.documentElement
-  const designWidth = opts.designWidth
-  const base = 100
-  let maxWidth = opts.maxWidth
   let clientWidth: number
 
   function handler() {
     clientWidth = $doc.clientWidth
 
-    // 以宽为标准，达到最大尺寸时，不变
+    // keep the width of client constant when reaching maxWidth
     if (maxWidth && (clientWidth >= maxWidth)) {
       clientWidth = maxWidth
     }
